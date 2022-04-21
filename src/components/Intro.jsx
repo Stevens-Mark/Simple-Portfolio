00c6a6ad9ab783d2b663c1212d21180cb55bf9b0
Mark Stevens <markspstevens@yahoo.co.uk>
2022-04-21 14:43:04 +0200

    skills progress bar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,8 +9,8 @@ const Introduction = () => {
         <div className="row gy-4 gy-md-0 align-items-center">
 
           <div className="col-12 col-md-6 ">
-            <h1 className="fw-bold">I'm John Doe, Web Developer and Designer</h1>
-            <h2 className="fw-light">Welcome to my creative universe</h2>
+            <h1 className="fw-bold title">I'm John Doe, Web Developer and Designer</h1>
+            <h2 className="fw-light slogan">Welcome to my creative universe</h2>
             <button type="button" className="btn btn-primary mt-5">My web skills</button>
           </div>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-// import 'bootstrap/dist/js/bootstrap.bundle';
-
 
 const Navigation = () => {
 
